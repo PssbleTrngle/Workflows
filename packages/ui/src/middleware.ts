8873fd6dca2f9d3d6ab684fa25b0ceeb4429bd54
Niklas Widmann <nik@somethingcatchy.net>
2026-05-05 18:11:12 +0200

    grab protocol from forwarded header

--- packages/ui/src/middleware.ts
+++ packages/ui/src/middleware.ts
@@ -4,10 +4,15 @@ import createApiClient from "./lib/api";
 
 const COOKIE = "webhooks-session";
 
+function getProtocol(request: Request) {
+  const forwarded = request.headers.get("x-forwarded-proto");
+  if (forwarded) return `${forwarded}:`;
+  const url = new URL(request.url);
+  return url.protocol;
+}
+
 const initializeContext = defineMiddleware(({ locals, request }, next) => {
-  console.log("requesting at ", request.url);
-  const { protocol } = new URL(request.url);
-  console.log(request.headers);
+  const protocol = getProtocol(request);
   const host = request.headers.get("host");
   if (!host) throw new Error("host header missing, cannot create api client");
   const origin = `${protocol}//${host}`;
